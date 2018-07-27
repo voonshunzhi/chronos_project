c316e09d094969a311ad7f12c03731375fde3031
teckhong <lthong96@gmail.com>
2018-07-27 12:26:37 +0800

    ajax for diabetes/obeses/hypertensions history

--- app/controllers/diabetes_controller.rb
+++ app/controllers/diabetes_controller.rb
@@ -1,21 +1,32 @@
 class DiabetesController < ApplicationController
+	def index
+		 patient = Patient.find(params[:id])
+        @history = patient.health_record.diabetes
+        respond_to do |format|
+            format.js { render "create" }
+        end
+	end
+
 	def new
+		# passed in patient.id as a params, to keep track of the patient, instead of current_user.id, to allow doctors to see the histories too.
+		@patient = Patient.find(params[:id])
 	    @diabete = Diabete.new
-	    @history = current_user.patient.health_record.diabetes
+		@history = @patient.health_record.diabetes
 	    respond_to do |format|
 	        format.js 
 	    end
 	end
 	
 	def create
+		patient = Patient.find(params[:id])
 	    @diabete = Diabete.new(diabete_params)
-	    @diabete.health_record_id = current_user.patient.health_record.id
+	    @diabete.health_record_id = patient.health_record.id
 	    if @diabete.save
 	        flash[:success] = "Record is successfully created."
 	    else
 	        flash[:danger] = "Record is not created."
 	    end
-	    @history = current_user.patient.health_record.diabetes
+	    @history = patient.health_record.diabetes
 	    respond_to do |format|
 	        format.js
 	    end
